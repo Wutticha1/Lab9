ccc1fc9c0a016974d755f2874c455cf1cdc8c3f9
Wutticha1 <66160032@go.buu.ac.th>
2025-02-16 15:07:52 +0700

    style เพิ่มฟังชั่นปุ่มให้กดแล้วมีคอมเมนต์

--- user-posts/index.js
+++ user-posts/index.js
@@ -27,12 +27,51 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (posts.length > 0) {
         postsList.innerHTML = posts.map(post => `
                 <div class = "card-item">
-                    <h2>${post.title}<h2>
-                    <p>${post.body}</p>
-                    <a href="user-comments/index.html?postId=${post.id}&userId=${userId}" id="comment">ดูความคิดเห็น</a>
+                    <h2>${post.title}</h2>
+                    <p class="body">${post.body}</p>
+                    <button class="toggle-comments" postId="${post.id}">ดูความคิดเห็น</button>
+                    <hr class="under-line" style="display:none">
+                    <div class="comment-container" id="comment-${post.id}" style="display:none"></div>
                 </div>
             `).join("");
         } else {
             postsTitle.innerHTML = "ไม่พบข้อมูลโพสต์";
         }
+
+    // ดึงปุ่มดูความคิดเห็น
+    document.querySelectorAll(".toggle-comments").forEach (button => {
+        button.addEventListener("click", async () => {
+            const postId = button.getAttribute("postId");
+            const commentContainer = document.getElementById(`comment-${postId}`)
+            const under = document.querySelector(".under-line");
+
+            if (commentContainer.style.display === "none") {
+                const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+                const comments = await response.json();
+
+                if (comments.length > 0) {
+                    commentContainer.innerHTML = comments.map(comment => `
+                            <div class="container-main">
+                                <div class="container-comment">
+                                    <h3>${comment.email}</h3>
+                                    <p>${comment.body}</p>
+                                </div>
+                            </div>
+                    `).join("");
+                } else {
+                    commentContainer.innerHTML = "ไม่มีความคิดเห็น";
+                }
+
+                under.style.display = "block";
+                commentContainer.style.display = "block";
+                
+                button.innerHTML = "ซ่อนความคิดเห็นนี้";
+
+            } else {
+                commentContainer.style.display ="none";
+                under.style.display = "none";
+                button.innerHTML = "ดูความคิดเห็น";
+            }
+        });
+    });
 });
