c4467955de5c1060dd0a97a3209875bfde030d01
Wutticha1 <66160032@go.buu.ac.th>
2025-02-16 09:06:12 +0700

    finish js in user-info

--- user-info/user-detail.js
+++ user-info/user-detail.js
@@ -19,9 +19,35 @@ document.addEventListener("DOMContentLoaded", async () => {
         // แสดงชื่อผู้ใช้
         userDetail.innerHTML = `
             <h2>${user.name}</h2>
-        
+            <div class="head-info">อีเมล</div>
+            <div class="p-info">${user.email}</div>
+
+            <div class="head-info">ชื่อผู้ใช้</div>
+            <div class="p-info">${user.username}</div>
+
+            <div class="head-info">เบอร์โทรศัพท์</div>
+            <div class="p-info">${user.phone}</div>
+
+            <div class="head-info">เว็บไซต์</div>
+            <div class="p-info">${user.website}</div>
+
+            <div class="head-info">ที่อยู่</div>
+            <p class="info">${user.address.street}</p>, 
+            <p class="info">${user.address.suite}</p><br> 
+            <p class="info">${user.address.city}</p>,
+            <p class="info">${user.address.zipcode}</p>
+
+            <div class="head-info">บริษัท</div>
+            <div class="p-info">${user.company.name}</div>
+            <div class="p-info">${user.company.catchPhrase}</div>
         `;
+
+        // เมื่อคลิกที่ปุ่ม "ดูโพสต์"
+        viewPostBtn.addEventListener("click", () => {
+            window.location.href = `./user-posts.html?userId=${userId}`;
+        })
+        
     } catch (error) {
         userDetail.innerHTML = '<p>เกิดข้อผิดพลาดในการโหลดข้อมูลผู้ใช้</p>';
     }
-})
+});
